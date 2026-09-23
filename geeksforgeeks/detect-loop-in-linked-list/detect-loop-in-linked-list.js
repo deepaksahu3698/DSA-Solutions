4aa0cfc5ad8782b2480cf03393d3718de64083ce
Deepak kumar sahu <82998111+deepaksahu3698@users.noreply.github.com>
2026-09-23 21:05:06 +0530

    Solution submitted successfully

--- geeksforgeeks/detect-loop-in-linked-list/detect-loop-in-linked-list.js
+++ geeksforgeeks/detect-loop-in-linked-list/detect-loop-in-linked-list.js
@@ -13,18 +13,34 @@ class Node{
 class Solution {
     detectLoop(head) {
         // code here
-        let map = new Map()
-        let temp = head
+        // let map = new Map()
+        // let temp = head
         
-        while(temp != null){
-            if(map.has(temp)){
+        // while(temp != null){
+        //     if(map.has(temp)){
+        //         return true
+        //     }
+            
+        //     map.set(temp)
+        //     temp = temp.next
+            
+        // }
+        // return false
+        let fast = head
+        let slow = head
+        while(fast != null && fast.next != null){
+            
+           
+            slow =slow.next
+            fast = fast.next.next
+            
+             if(slow == fast){
                 return true
             }
             
-            map.set(temp)
-            temp = temp.next
-            
         }
         return false
+        
+        
     }
 }
